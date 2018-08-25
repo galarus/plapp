9324acc2380edcd573d63ff429fc2d65c4429f91
galarus <dwmarx@ucsc.edu>
2018-08-24 17:48:45 -0700

    database is working with rax in browser

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import {createElement, Component} from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
 import styles from './App.css';
+import plant_data from './plapp_data.js';
 
+var Datastore = require('nedb')
+  , db = new Datastore();
+db.insert(plant_data, function(err, newDocs){
+	//console.log(newDocs);
+})
 class App extends Component {
+  componentWillMount() {
+  	db.find({plant_genus: "Cotoneaster"}, function (err, docs){
+  		console.log(docs);
+  	})
+  }
   render() {
     return (
       <View style={styles.app}>
@@ -11,7 +22,7 @@ class App extends Component {
           <Text style={styles.appBanner}>Welcome to Rax</Text>
         </View>
         <Text style={styles.appIntro}>
-          To get started, edit src/App.js and save to reload.
+          To get started, edit src/App.js and save to reload. omg
         </Text>
       </View>
     );
