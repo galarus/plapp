a9ff768cc2a224edf678756c55b44ef7493245dd
Derek Marx <derekwmarx@hotmail.com>
2018-09-01 21:46:46 -0700

    fixed the scroll orientation and refreshing of the flatlist

--- PlantList.js
+++ PlantList.js
@@ -1,8 +1,7 @@
 
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, ScrollView, FlatList, RefreshControl} from 'react-native';
-
-class PlantListItem extends Component<Props>{
+class PlantListHeader extends Component<Props>{
     constructor(props){
         super(props);
         this.state = {};
@@ -12,6 +11,21 @@ class PlantListItem extends Component<Props>{
        return (
            <View
            style={{padding:20}}>
+<Text>JEPSON CODE</Text>
+</View>
+        )
+    }
+}
+class PlantListItem extends Component<Props>{
+    constructor(props){
+        super(props);
+        this.state = {};
+       // console.log(this.props.item)
+    }
+    render(){
+       return (
+           <View
+           style={{height:30}}>
 <Text>{this.props.item.jepson_code}</Text>
 </View>
         )
@@ -21,8 +35,15 @@ class PlantList extends Component<Props> {
     constructor(props){
         super(props);
         //sorting will be stored in this state
+     //   let fullList =this.props.searchResults;
+      //  let limit
+      //  if (fullList.length >= 15)
+      //  let initialList = fullList.length >= 15 ? 
+        //                fullList.slice(0, 15)
+          //              :   
+
         this.state = {
-            renderedList: [], 
+            renderedList:[] , 
             offset: 0,
             refreshing: false
         };
@@ -32,7 +53,7 @@ class PlantList extends Component<Props> {
     _updateList = () => {
         let { offset, renderedList } = this.state;
         let fullList = this.props.searchResults;
-        let limit = offset + 15;
+        let limit = offset + 20;
         console.log("updateList() " + offset + " try " + limit);
         if ( fullList.length >= limit ) {
             let newItems = fullList.slice(offset, limit);
@@ -58,25 +79,41 @@ class PlantList extends Component<Props> {
         }
     }
     _onRefresh = () => {
+        return new Promise((resolve) => {
+
         this.setState({refreshing: true});
         this._updateList().then(() => {
-          this.setState({refreshing: false});
+                this.setState({
+                    refreshing: false
+                }, () =>{
+                    resolve() 
+                })
+            });
         });
       }
     componentDidMount(){
         console.log("platlist mounted")
-        this._onRefresh();
+       this._onRefresh().then(() => {
+           console.log("scroll plz");
+         setTimeout(()=>this.flatList.scrollToIndex({index:19 , animated: false}), 1);
+        }
+       );
     }
     
     render(){
 return(
     <View>
+    <PlantListHeader/>
         <FlatList
+        ref={flatList => this.flatList = flatList}
+        inverted
+        getItemLayout={(data, index) => (
+  {length: 30, offset: 30 * index, index}
+)}
   data={this.state.renderedList}
-  renderItem={({item}) => <PlantListItem item={item}/>}
+  renderItem={({item, index}) => <PlantListItem item={item}/>}
   keyExtractor={(item)=> item.jepson_code.toString()}
-  onEndReached={this._onRefresh}
-            onEndReachedThreshold={0.1}
+  onEndReachedThreshhold={0.5}
             refreshControl={
           <RefreshControl
             refreshing={this.state.refreshing}
