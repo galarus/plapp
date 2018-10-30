48d89b5efc71dc45ea35f5714d0d2794f21a0994
galarus <dwmarx@ucsc.edu>
2018-10-29 23:58:01 -0700

    footer hover and click animation

--- src/Footer/PlantFooter.js
+++ src/Footer/PlantFooter.js
@@ -4,20 +4,7 @@ import './PlantFooter.css';
 
 const PlantFooter = () => (
   <div className="App">
-    <div
-      style={{
-        padding: '2em',
-        width: '100%',
-        position: 'fixed',
-        bottom: '0',
-        backgroundColor: '#891d00',
-        color: '#f1f1f1',
-        fontWeight: 'bold',
-        flex: 1
-      }}
-    >
-      Search ^
-    </div>
+    <div className="footer">Search ^</div>
   </div>
 );
 
