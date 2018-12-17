32b5da302380ce3369fe38b84a4c6959cc904a51
galarus <dwmarx@ucsc.edu>
2018-12-16 16:33:21 -0800

    responsive grid for leaf search

--- src/Components/Search/SearchContent.js
+++ src/Components/Search/SearchContent.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import './SearchContent.css';
 
 type Shapes = {
   ovate: boolean,
@@ -34,145 +35,133 @@ function SearchContent(props: Props) {
   const { searchQuery, onSearchChange } = props;
   const { shapes, forms, arrangements } = searchQuery;
   return (
-    <div
-      style={{
-        textAlign: 'left',
-        marginLeft: '10%',
-        marginRight: '10em',
-        marginTop: '1em',
-        width: '60vw',
-        height: '50vh',
-        overflow: 'auto',
-        padding: '0.2em 2em 2em 2em',
-        backgroundColor: '#4FA2BE',
-        borderWidth: '2px',
-        borderStyle: 'inset'
-      }}
-    >
-      <h3>Leaf Shape</h3>
-      <form>
-        <div>
-          ovate:
+    <div className="search-content">
+      <h3 className="form-title">Leaf Shape</h3>
+      <form className="form-content">
+        <div className="form-item">
           <input
             name="ovate"
             type="checkbox"
             checked={shapes.ovate}
             onChange={onSearchChange('shape')}
           />
+          ovate:
+          <p>ergseg srtg sg wth eyjeyhdfgh q4gt adfwyh rths hsrths r</p>
         </div>
-        <div>
-          lanceolate:
+        <div className="form-item">
           <input
             name="lanceolate"
             type="checkbox"
             checked={shapes.lanceolate}
             onChange={onSearchChange('shape')}
           />
+          lanceolate:
         </div>
-        <div>
-          obovate:
+        <div className="form-item">
           <input
             name="obovate"
             type="checkbox"
             checked={shapes.obovate}
             onChange={onSearchChange('shape')}
           />
+          obovate:
         </div>
-        <div>
-          cordate:
+        <div className="form-item">
           <input
             name="cordate"
             type="checkbox"
             checked={shapes.cordate}
             onChange={onSearchChange('shape')}
           />
+          cordate:
         </div>
-        <div>
-          linear:
+        <div className="form-item">
           <input
             name="linear"
             type="checkbox"
             checked={shapes.linear}
             onChange={onSearchChange('shape')}
           />
+          linear:
         </div>
       </form>
-      <h3>Leaf Arrangement</h3>
-      <form>
-        <div>
-          basal:
+      <br />
+      <h3 className="form-title">Leaf Arrangement</h3>
+      <form className="form-content">
+        <div className="form-item">
           <input
             name="basal"
             type="checkbox"
             checked={arrangements.basal}
             onChange={onSearchChange('arrangement')}
           />
+          basal:
         </div>
-        <div>
-          whirled:
+        <div className="form-item">
           <input
             name="whirled"
             type="checkbox"
             checked={arrangements.whirled}
             onChange={onSearchChange('arrangement')}
           />
+          whirled:
         </div>
-        <div>
-          alternate:
+        <div className="form-item">
           <input
             name="alternate"
             type="checkbox"
             checked={arrangements.alternate}
             onChange={onSearchChange('arrangement')}
           />
+          alternate:
         </div>
-        <div>
-          opposite:
+        <div className="form-item">
           <input
             name="opposite"
             type="checkbox"
             checked={arrangements.opposite}
             onChange={onSearchChange('arrangement')}
           />
+          opposite:
         </div>
       </form>
-      <h3>Form</h3>
-      <form>
-        <div>
-          grass:
+      <h3 className="form-title">Form</h3>
+      <form className="form-content">
+        <div className="form-item">
           <input
             name="grass"
             type="checkbox"
             checked={forms.grass}
             onChange={onSearchChange('form')}
           />
+          grass:
         </div>
-        <div>
-          forb:
+        <div className="form-item">
           <input
             name="forb"
             type="checkbox"
             checked={forms.forb}
             onChange={onSearchChange('form')}
           />
+          forb:
         </div>
-        <div>
-          tree:
+        <div className="form-item">
           <input
             name="tree"
             type="checkbox"
             checked={forms.tree}
             onChange={onSearchChange('form')}
           />
+          tree:
         </div>
-        <div>
-          parasite:
+        <div className="form-item">
           <input
             name="parasite"
             type="checkbox"
             checked={forms.parasite}
             onChange={onSearchChange('form')}
           />
+          parasite:
         </div>
       </form>
     </div>
